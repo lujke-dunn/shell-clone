03f4241b8431a74f1483c0f3e247051c05f3b4e0
lujke-dunn <lukedunn011@gmail.com>
2025-02-12 12:00:00 +1100

    codecrafterrrrrs

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <cstdlib>
 #include <sstream>
+#include <iostream>
+#include <cstdlib>
+#include <unistd.h>
+#include <pwd.h>
+#include <sstream>
 #include <string>
 #include <filesystem>
 
@@ -9,7 +14,7 @@ std::string get_path(std::string command) {
   std::string path; 
 
   std::stringstream ss(path_env);
-
+  
   while (!ss.eof()) {
     getline(ss, path, ':'); 
     std::string absolute_path = path + "/" + command; 
@@ -22,22 +27,44 @@ std::string get_path(std::string command) {
   return ""; 
 }
 
-void cd_command(const std::string& input) {
+std::string get_username() {
+   struct passwd *pw = getpwuid(getuid());
 
-   std::string path = input.substr(input.find_first_of(" ") + 1);
-   
-   try {
-     std::filesystem::current_path(path); 
-   } catch (const std::filesystem::filesystem_error&) {
-     std::cerr << "cd: " << path << ": No such file or directory" << '\n';
-   }
+   return pw ? pw->pw_name : ""; 
 }
 
+
+void cd_command(const std::string& input) {
+    std::string path = input.substr(input.find_first_of(" ") + 1);
+    
+    if (!path.compare("~")) {
+        const char* home = std::getenv("HOME");
+        std::string homepath;
+        
+        if (home != nullptr) {
+            homepath = std::string(home);
+        } else {
+            homepath = "/home/" + get_username();
+        }
+        
+        try {
+            std::filesystem::current_path(homepath);
+        } catch (const std::filesystem::filesystem_error&) {
+            std::cerr << "cd: " << homepath << ": No such file or directory" << '\n';
+        }
+    } else {  
+        try {
+            std::filesystem::current_path(path);
+        } catch (const std::filesystem::filesystem_error&) {
+            std::cerr << "cd: " << path << ": No such file or directory" << '\n';
+        }
+    }
+}
 
 int execute_command(const std::string& command) {
   return system(command.c_str()); 
 }
-// add this tmr 
+
 void pwd_command() { 
    std::string current_path { std::filesystem::current_path() };
    std::cout << current_path << std::endl; 
@@ -54,11 +81,54 @@ bool type_command(std::string command) {
 }
 
 
-void echo_command(std::string sentence) { 
-  sentence = sentence.substr(sentence.find_first_of(" \t") + 1); 
-
-  std::cout << sentence << '\n'; 
-} 
+void echo_command(std::string sentence) {
+    sentence = sentence.substr(sentence.find_first_of(" \t") + 1);
+    
+    std::string result;
+    char quoteChar = 0;
+    bool escaped = false;
+    
+    for (int i = 0; i < sentence.length(); i++) {
+        char currentChar = sentence[i];
+        
+        if (escaped && quoteChar != '\'') {
+            result += currentChar;
+            escaped = false;
+            continue;
+        }
+        
+        if (currentChar == '\\' && quoteChar != '\'') {
+            escaped = true;
+            continue;
+        }
+        
+        if (currentChar == '\'' || currentChar == '"') {
+            if (quoteChar == 0) {
+                quoteChar = currentChar;
+            } else if (currentChar == quoteChar) {
+                quoteChar = 0;
+            } else {
+                result += currentChar;
+            }
+            continue;
+        }
+        
+        if (std::isspace(currentChar) && quoteChar == 0) {
+            if (!result.empty() && !std::isspace(result.back())) {
+                result += ' ';
+            }
+        } else {
+            result += currentChar;
+        }
+    }
+    
+    if (!result.empty() && std::isspace(result.back())) {
+        result.pop_back();
+    }
+    
+    std::cout << result << '\n';
+}    
+     
 
 void exit_command() {
   return std::_Exit(EXIT_SUCCESS); 
